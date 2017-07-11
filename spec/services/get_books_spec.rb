76d0e5bfad03014f2e5fc39d307f4839720e3092
Nicole Santamaria and Sara Schultz <sara@>
2017-07-11 11:16:39 -0700

    install vcr gem and add info to rails_helper

--- spec/services/get_books_spec.rb
+++ spec/services/get_books_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Book do
+describe Book, :vcr => true do
   it "returns a 200 success header when the API call is made" do
     response = Book.get_books
     expect(response.code).to(eq(200))
